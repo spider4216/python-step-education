b965e96bffdd5c364b3c8a214d04b366682e1504
Yuriy Sirotenko <yosirotenko@gmail.com>
2020-05-28 11:21:35 +0600

    done

---
+++ lesson_27_05_2020/simple_math.py
@@ -0,0 +1,17 @@
+x = int(input('Enter x: '))
+y = int(input('Enter y: '))
+
+res = x - y
+
+print('difference: ', res)
+
+res = x * y;
+
+print('multiplication: ', res)
+
+try:
+    res = x / y
+except:
+    res = 'Cannot be devided by zero';
+
+print('division: ', res)
